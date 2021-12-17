f46e160b22822723949004df6bcd2175f7f78eae
Ricardo López Alvarez <rlopez@dgraficos.com>
2021-12-17 22:32:54 +0100

    Challenge #10

--- 10.js
+++ 10.js
@@ -1,25 +1,26 @@
 export default function getCoins(change) {
-  let changeArray = []
-  do{
-    if(change>50){
-      changeArray[5]++
-      change -= 50
-    } else if(change>20) {
-      changeArray[4]++
-      change -= 20
-    } else if(change>10) {
-      changeArray[3]++
-      change -= 10
-    } else if(change>5) {
-      changeArray[2]++
-      change -= 5
-    } else if(change>2) {
-      changeArray[1]++
-      change -= 2
-    } else if(change>1) {
-      changeArray[0]++
-      change -= 1
-    }
-  }while(change>0)
-  return changeArray
+	let changeArray = [0, 0, 0, 0, 0, 0]
+	let change = change
+	do {
+		if (change >= 50) {
+			changeArray[5]++
+			change -= 50
+		} else if (change >= 20) {
+			changeArray[4]++
+			change -= 20
+		} else if (change >= 10) {
+			changeArray[3]++
+			change -= 10
+		} else if (change >= 5) {
+			changeArray[2]++
+			change -= 5
+		} else if (change >= 2) {
+			changeArray[1]++
+			change -= 2
+		} else if (change >= 1) {
+			changeArray[0]++
+			change -= 1
+		}
+	} while (change > 0)
+	return changeArray
 }
